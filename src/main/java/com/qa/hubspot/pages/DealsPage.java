0e56e8a0ab4858ea3fcd9176c22559305c42ea70
Venkattestauto <venkatmtech.testing@gmail.com>
2019-09-25 16:03:11 +0530

    created deals page class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,18 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.Base.BasePage;
+
+
+public class DealsPage extends BasePage {
+	
+	public DealsPage(WebDriver driver){
+  	  this.driver = driver;  
+  	  
+    }
+	
+	
+	
+
+}
